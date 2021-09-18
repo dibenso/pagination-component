c85b4ab9e7a7d8a3d644fdca7a3dd869797eccbf
dibenso <dibenso@users.noreply.github.com>
2021-09-18 16:10:18 +0000

    Deploying to main from @ dibenso/pagination-component@8b9c8821daeaeb1c12a3da80f508323347007481 🚀

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import Pagination from "./Pagination/Pagination";
-import { PaginationProps, PaginationRenderProps, SetPageOptions } from "./Pagination/types";
-
-export default Pagination;
-export type { PaginationProps, PaginationRenderProps, SetPageOptions };
